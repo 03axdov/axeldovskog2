bcd6f8eb4b974d0bbfc9ee67a28cb914ebe3a71c
axeld <axel.dovskog@outlook.com>
2025-08-10 12:45:54 +0200

    Some improvements

---
+++ src/components/DalinarPage.tsx
@@ -0,0 +1,109 @@
+import { useState } from "react"
+
+type ImageType = 1 | 2 | 3 | 4
+
+export default function DalinarPage() {
+
+    const [currentImages, setCurrentImages] = useState<ImageType[]>([1, 2, 3, 4])
+
+    function imageOnClick(clickedIdx: number) {
+        let newPrimary = currentImages[clickedIdx]
+        let temp = [...currentImages]
+        temp[clickedIdx] = temp[0]
+        temp[0] = newPrimary
+        setCurrentImages(temp)
+    }
+
+    return (
+        <div className="flex flex-col">
+            <a target="_blank" href="https://www.dalinar.net" className="w-full text-4xl pb-3 flex flex-row items-center tracking-widest border-b border-gray-600 mb-5 text-gray-200">
+                DALINAR
+                <i className="fa-solid fa-arrow-up-right-from-square text-gray-400 text-sm ml-3" />
+            </a>
+            
+            <div className="w-[full] flex flex-col items-center gap-2 pb-5 border-b border-gray-600">
+                <img className="w-[80%] rounded-lg h-full object-cover" src={`/static/images/portfolio/dalinar${currentImages[0]}.jpg`} />
+
+                <div className="w-[80%] grid grid-cols-3 gap-2">
+                    <img className="w-full aspect-[4/3] object-cover rounded-lg cursor-pointer" 
+                    onClick={() => imageOnClick(1)} 
+                    src={`/static/images/portfolio/dalinar${currentImages[1]}.jpg`} />
+
+                    <img className="w-full aspect-[4/3] object-cover rounded-lg cursor-pointer" 
+                    onClick={() => imageOnClick(2)} 
+                    src={`/static/images/portfolio/dalinar${currentImages[2]}.jpg`} />
+
+                    <img className="w-full aspect-[4/3] object-cover rounded-lg cursor-pointer" 
+                    onClick={() => imageOnClick(3)}
+                    src={`/static/images/portfolio/dalinar${currentImages[3]}.jpg`} />
+                </div>
+            </div>
+
+            <p className="mt-10 text-lg text-gray-400">
+                A website that allows users to create machine learning datasets and models without having to code.
+                Users are then able to export models or train them on datasets, evaluate models, and use the models to predict data.<br></br>
+                This project taught me a lot about working on large-scale projects, using APIs for more complex scenarios, e.g. using tasks, as well as web development & machine-learning in general.
+            </p>
+            <a target="_blank" href="https://www.dalinar.net" 
+            className="flex flex-row items-center justify-center rounded-full p-2 px-6 bg-blue-500 hover:bg-blue-600 cursor-pointer mt-5 mr-auto">
+                Check out website
+                <i className="fa-solid fa-arrow-up-right-from-square text-sm ml-3" />
+            </a>
+
+            <p className="text-xl w-full mt-10 mb-2 text-gray-200">Tutorial</p>
+            <p className="mb-5 text-lg text-gray-400">
+                Here's a brief tutorial, please excuse the audio :)
+            </p>
+
+            <div style={{ position: "relative", paddingBottom: "56.25%", height: 0}}>
+                <iframe
+                    src="https://www.youtube.com/embed/tQ2lUxumQV4"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    title="Embedded YouTube"
+                    className="border border-gray-600 rounded-lg"
+                    style={{
+                    position: "absolute",
+                    top: 0,
+                    left: "10%",
+                    width: "80%",
+                    height: "80%"
+                    }}
+                ></iframe>
+            </div>
+
+            <p className="text-xl w-full text-gray-200 pb-1 border-b border-gray-600">Frameworks and technologies used</p>
+            <div className="mt-5 flex flex-row flex-wrap w-full gap-3">
+                <p className="text-lg p-1 px-3 text-md flex flex-row items-center rounded-md border border-yellow-400 text-yellow-400">
+                    <i className="fa-brands fa-js fa-lg mr-3"></i>
+                    Javascript
+                </p>
+                <p className="text-lg p-1 px-3 text-md flex flex-row items-center rounded-md border border-blue-400 text-blue-400">
+                    <i className="fa-brands fa-python fa-lg mr-3"></i>
+                    Python
+                </p>
+                <p className="text-lg p-1 px-3 text-md flex flex-row items-center rounded-md border border-cyan-400 text-cyan-400">
+                    <i className="fa-brands fa-react fa-lg mr-3"></i>
+                    Javascript React
+                </p>
+                <p className="text-lg p-1 px-3 text-md flex flex-row items-center rounded-md border border-green-300 text-green-300">
+                    <i className="fa-brands fa-python fa-lg mr-3"></i>
+                    Python Django
+                </p>
+                <p className="text-lg p-1 px-3 text-md flex flex-row items-center rounded-md border border-orange-400 text-orange-400">
+                    <i className="fa-brands fa-google fa-lg mr-3"></i>
+                    Tensorflow
+                </p>
+                <p className="text-lg p-1 px-3 text-md flex flex-row items-center rounded-md border border-purple-400 text-purple-400">
+                    <i className="fa-brands fa-salesforce fa-lg mr-3"></i>
+                    Heroku
+                </p>
+                <p className="text-lg p-1 px-3 text-md flex flex-row items-center rounded-md border border-orange-400 text-orange-400">
+                    <i className="fa-brands fa-aws fa-lg mr-3"></i>
+                    AWS
+                </p>
+            </div>
+        </div>
+    )
+}
--- src/views/Portfolio.tsx
+++ src/views/Portfolio.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import DalinarPage from "../components/DalinarPage"
+import MuskiePage from "../components/MuskiePage"
 
 type ProjectType = "Dalinar" | "Muskie" | "MediaReviewr"
 
@@ -11,7 +13,7 @@ export default function Portfolio() {
     ) {
         return (
             <div className={"flex w-full rounded-lg flex-row items-center justify-center cursor-pointer " +
-                (selectedSection == section ? "border border-" + selectedColor : "border border-[rgb(0,0,0,0)]")
+                (selectedSection == section ? "border border-" + selectedColor : "border border-[rgb(0,0,0,0)] hover:border-gray-600")
             }
             style={{
                 background: background
@@ -30,15 +32,21 @@ export default function Portfolio() {
                 I enjoy working on my own projects in my spare time. Here are the main ones.
             </p>
             
-            <div className="flex flex-row mt-20 justify-center gap-x-15">
-                <div className="flex flex-col items-center gap-y-5">
-                    {getProjectElement("Dalinar.jpg", "Dalinar", "#0d1017", "blue-300")}
-                    {getProjectElement("Muskie.jpg", "Muskie", "#0b1016", "cyan-400")}
+            <div className="flex flex-row mt-20 justify-center gap-x-15 relative">
+                <div className="flex flex-col items-center gap-y-5 sticky top-0 w-[300px] min-w-[300px]">
+                    {getProjectElement("Dalinar.jpg", "Dalinar", "#18191c", "blue-300")}
+                    {getProjectElement("Muskie.jpg", "Muskie", "#18191c", "cyan-400")}
                     {getProjectElement("Mediareviewr.jpg", "MediaReviewr", "#18191c", "purple-400")}
                 </div>
 
                 <div className="max-w-[1000px]">
-                    
+                    {selectedSection == "Dalinar" && <DalinarPage />}
+
+                    {selectedSection == "Muskie" && <MuskiePage />}
+
+                    {selectedSection == "MediaReviewr" && <div>
+                        
+                    </div>}
                 </div>
             </div>
         </div>
